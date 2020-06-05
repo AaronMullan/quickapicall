82e247edde61462ba9f0078a20289e47d921d09f
AaronMullan <aaroncmullan@gmail.com>
2020-06-05 15:06:12 -0700

    live data rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {
+  VictoryLabel, VictoryLine,
+} from 'victory';
 import './App.css';
+import data from './jaildata';
+
+// useEffect(() => fetch('https://swapi.co/api/planets/4/')
+//   .then((res) => res.json())
+//   .then((res) => this.setState({ planets: res }))
+//   .catch(() => this.setState({ hasErrors: true })));
 
 function App() {
+  const [jailData, setJailData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://mult-co-jail-data.herokuapp.com/api/v1/dailyCounts') //eslint-disable-line
+      .then((res) => res.json())
+      .then((res) => setJailData(res.counts.slice(5)));
+    setIsLoading(false);
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <div className="App">
+        <div className="Container">
+          <h1>blah blah</h1>
+          <div className="Chart">
+
+
+            <VictoryLine
+            // labelComponent={<VictoryTooltip />}
+              data={jailData}
+              // labels={({ datum }) => datum.count}
+              labelComponent={<VictoryLabel renderInPortal dy={-10} />}
+              x="date"
+              y="count"
+            />
+
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>loading</h1>
   );
 }
-
 export default App;
